36b69812d78fdd1d3c82e26f5ac9d2128b02fd38
priyanka897-star <priyankamore8997@gmail.com>
2021-09-26 23:47:16 +0530

    Added:Functionality to check equality of two lines

--- LineComparision/LineComparision/src/com/bridgelab/linecomparision/LineComparision.java
+++ LineComparision/LineComparision/src/com/bridgelab/linecomparision/LineComparision.java
@@ -10,7 +10,16 @@ public class LineComparision {
 		}
    public static void main(String[] args) {
               LineComparision Object = new LineComparision();
-              double computeDistance = Object.distance(1,2,3,4);
-	          System.out.println("Distance between two point is :" +computeDistance);
-}
+              Double line1 = Object.distance(1,2,3,4);
+              Double line2 = Object.distance(1,2,3,4);
+              if (line1.equals(line2)) {
+                  System.out.println("Line1 and Line2 are equal");
+              }
+              else {
+            	  System.out.println("Line1 and Line2 are not equal");
+            	  
+               }
+ 
+   }
+              
 }
