9827e2a28af525a901197d6e9c19b7644b9db261
priyanka897-star <priyankamore8997@gmail.com>
2021-09-26 22:55:53 +0530

    Added:Functionality to Print Welcome Message

---
+++ LineComparision/LineComparision/src/com/bridgelab/linecomparision/LineComparion.java
@@ -0,0 +1,9 @@
+package com.bridgelab.linecomparision;
+
+public class LineComparion {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision Computation Program");
+		
+	}
+
+}
